99cfb84bb3457efcdb0bef818df507232d6aedd1
Duncan Kimutai <dunkim444@gmail.com>
2023-10-23 21:26:19 +0000

    setup

---
+++ packages/react-app/pages/_app.tsx
@@ -0,0 +1,42 @@
+import { Alfajores, Celo } from "@celo/rainbowkit-celo/chains";
+import celoGroups from "@celo/rainbowkit-celo/lists";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+import { InjectedConnector } from "wagmi/connectors/injected";
+
+
+
+const { chains, publicClient } = configureChains(
+  [Celo],
+  [publicProvider()]
+);
+
+const connectors = [new InjectedConnector({chains})]
+
+const appInfo = {
+  appName: "Celo Composer",
+};
+
+const wagmiConfig = createConfig({
+  connectors,
+  publicClient: publicClient,
+});
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains} appInfo={appInfo} coolMode={true}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default App;
